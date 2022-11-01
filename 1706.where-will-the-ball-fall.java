58c660de4d84a3305eb277ef6068db52b5a4f2b3
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-01 23:30:35 +0530

    Daily coding challenge November 1, 2022

---
+++ 1706.where-will-the-ball-fall.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1706 lang=java
+ *
+ * [1706] Where Will the Ball Fall
+ */
+
+// @lc code=start
+class Solution {
+    public int[] findBall(int[][] grid) {
+        
+    }
+}
+// @lc code=end
+
+/*
+ * [[1,1,1,-1,-1],
+ *  [1,1,1,-1,-1],
+ *  [-1,-1,-1,1,1],
+ *  [1,1,1,1,-1],
+ *  [-1,-1,-1,-1,-1]]
+ */
---
+++ 91.decode-ways.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=91 lang=java
+ *
+ * [91] Decode Ways
+ */
+
+// @lc code=start
+class Solution {
+    public int numDecodings(String s) {
+
+        
+    }
+}
+// @lc code=end
+
