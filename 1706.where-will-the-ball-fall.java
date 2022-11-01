3b28c3f7e37b69c307aea99bdc3c27bf046f391c
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-01 23:31:52 +0530

    modified:   1706.where-will-the-ball-fall.java

--- 1706.where-will-the-ball-fall.java
+++ 1706.where-will-the-ball-fall.java
@@ -6,8 +6,37 @@
 
 // @lc code=start
 class Solution {
-    public int[] findBall(int[][] grid) {
-        
+    public int[] findBall(int[][] a) {
+        int n=a.length;
+        int m=a[0].length;
+        int arr[]=new int[m];
+        Arrays.fill(arr,-1);
+        for(int i=0;i<m;i++){
+            int x,y;
+            x=0;y=i;
+            while(true){
+                if(x>=n){
+                    if(y>=0 && y<m){
+                        arr[i]=y;
+                    }else{
+                        
+                    }
+                    break;
+                }
+                else{
+                    if(y+1<m && a[x][y]==1 && a[x][y+1]==1){
+                        x++;
+                        y++;
+                    }else if(y-1>=0 && a[x][y]==-1 && a[x][y-1]==-1){
+                        x++;
+                        y--;
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }
+        return arr;
     }
 }
 // @lc code=end
