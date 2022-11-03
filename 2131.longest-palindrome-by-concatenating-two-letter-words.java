973a1cdf0c6c34ae92c088251bfdb54ac7f0f84f
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-03 23:24:34 +0530

    modified:   .gitignore 	new file:   2131.longest-palindrome-by-concatenating-two-letter-words.java

---
+++ 2131.longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=2131 lang=java
+ *
+ * [2131] Longest Palindrome by Concatenating Two Letter Words
+ */
+
+// @lc code=start
+class Solution {
+    public int longestPalindrome(String[] words) {
+        
+}
+// @lc code=end
+
