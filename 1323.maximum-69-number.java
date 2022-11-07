5810724e83e19c40c8d7fc6c9b50706850a007fb
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-07 19:19:09 +0530

    new file:   1323.maximum-69-number.java Daily code: November 7, 2022

---
+++ 1323.maximum-69-number.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1323 lang=java
+ *
+ * [1323] Maximum 69 Number
+ */
+
+// @lc code=start
+class Solution {
+    public int maximum69Number (int num) {
+        char[] nums = Integer.toString(num).toCharArray();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == '6') {
+                nums[i] = '9';
+                break;
+            }
+        }
+
+        return Integer.parseInt(new String(nums));
+    }
+}
+// @lc code=end
+
