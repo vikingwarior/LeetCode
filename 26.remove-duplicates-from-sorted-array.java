fa980e3847ad8e212e85d816266b973d10a52d45
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-11 23:53:12 +0530

    new file:   26.remove-duplicates-from-sorted-array.java Daily Code: November 11, 2022

---
+++ 26.remove-duplicates-from-sorted-array.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=26 lang=java
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int insertIndex = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i - 1] != nums[i]) {
+                nums[insertIndex] = nums[i];     
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+}
+// @lc code=end
