400d17dbb394bcf44ba62811d071c6a873a59a13
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-18 20:09:58 +0530

    new file:   263.ugly-number.java

---
+++ 263.ugly-number.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=263 lang=java
+ *
+ * [263] Ugly Number
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isUgly(int num) {
+        if (num <= 0) {return false;}
+        if (num == 1) {return true;}
+        if (num % 2 == 0) {
+            return isUgly(num/2);
+        }
+        if (num % 3 == 0) {
+            return isUgly(num/3);
+        }
+        if (num % 5 == 0) {
+            return isUgly(num/5);
+        }
+        return false;
+    }
+}
+// @lc code=end
+
