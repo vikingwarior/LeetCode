4b8ad6fb158f4b9e26511cdde1775b8e642d7df4
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-20 23:09:50 +0530

    new file:   224.basic-calculator.java

---
+++ 224.basic-calculator.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=224 lang=java
+ *
+ * [224] Basic Calculator
+ */
+
+// @lc code=start
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                number = 10 * number + (int)(c - '0');
+            }else if(c == '+'){
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            }else if(c == '-'){
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            }else if(c == '('){
+                stack.push(result);
+                stack.push(sign);
+                sign = 1;   
+                result = 0;
+            }else if(c == ')'){
+                result += sign * number;  
+                number = 0;
+                result *= stack.pop();
+                result += stack.pop();
+                
+            }
+        }
+        if(number != 0) result += sign * number;
+        return result;
+    }
+}
+// @lc code=end
+
