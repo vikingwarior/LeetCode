3698f931d939b900c31b9d68b0e15ebb3462da49
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-22 22:24:16 +0530

    new file:   279.perfect-squares.java

---
+++ 279.perfect-squares.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=279 lang=java
+ *
+ * [279] Perfect Squares
+ */
+
+// @lc code=start
+public class Solution {
+    public int numSquares(int n) {
+       int[] dp = new int[n + 1];
+       Arrays.fill(dp, Integer.MAX_VALUE);
+       dp[0] = 0;
+       for(int i = 0; i <= n; i++){
+           for(int j = 1; i + j * j <= n; j++){
+               dp[i  + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+            }
+       }
+       return dp[n];
+    }
+}
+// @lc code=end
+
