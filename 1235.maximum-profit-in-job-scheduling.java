9001bed3acb291238194d1823f900987252bd94d
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-26 17:06:26 +0530

    new file:   1235.maximum-profit-in-job-scheduling.java

---
+++ 1235.maximum-profit-in-job-scheduling.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1235 lang=java
+ *
+ * [1235] Maximum Profit in Job Scheduling
+ */
+
+// @lc code=start
+class Solution {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
+        }
+        Arrays.sort(jobs, (a, b)->a[1] - b[1]);
+        TreeMap<Integer, Integer> dp = new TreeMap<>();
+        dp.put(0, 0);
+        for (int[] job : jobs) {
+            int cur = dp.floorEntry(job[0]).getValue() + job[2];
+            if (cur > dp.lastEntry().getValue())
+                dp.put(job[1], cur);
+        }
+        return dp.lastEntry().getValue();
+    }
+}
+// @lc code=end
+
