d8cd41c1aa874af5e0849542c4cea5c2fec8af3e
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-27 14:57:56 +0530

    new file:   446.arithmetic-slices-ii-subsequence.java

---
+++ 446.arithmetic-slices-ii-subsequence.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=446 lang=java
+ *
+ * [446] Arithmetic Slices II - Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        HashMap<Long, Integer>[] dp = new HashMap[n];
+        for (int i = 0; i < n; ++i) dp[i] = new HashMap<>();
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                long diff = (long) nums[i] - (long) nums[j];
+                int cnt = dp[j].getOrDefault(diff, 0);
+                dp[i].put(diff, dp[i].getOrDefault(diff, 0) + cnt + 1);
+                ans += cnt;
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
