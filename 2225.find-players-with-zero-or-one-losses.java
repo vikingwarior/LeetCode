5c978b098a3df4f3b24d9a2363ace4265a0fbca4
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-28 23:14:51 +0530

    modified:   2225.find-players-with-zero-or-one-losses.java with additional comments for better understanding

--- 2225.find-players-with-zero-or-one-losses.java
+++ 2225.find-players-with-zero-or-one-losses.java
@@ -11,23 +11,28 @@ import java.util.Map;
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
       
-      Map<Integer, Integer> lossCount = new HashMap<>();
+      Map<Integer, Integer> lossCount = new HashMap<>(); // stores the data of all the losers
       Set<Integer> winnerSet = new HashSet<>();
       Set<Integer> loserSet = new HashSet<>();
       
+      // Count losses for all the losers
       for (int i = 0; i < matches.length; i++) {
         if(lossCount.containsKey(matches[i][1])) lossCount.replace(matches[i][1], lossCount.get(matches[i][1])+1);
         else lossCount.put(matches[i][1], 1);
       }
 
+      
       for (int i = 0; i < matches.length; i++) {
+            // Checks if winner exists in Map of losers
             if (!lossCount.containsKey(matches[i][0])) {
                 winnerSet.add(matches[i][0]);
             }
 
+            // Retirives all the losers with 1 loss and stores their value seperately
             if(lossCount.get(matches[i][1]) == 1) loserSet.add(matches[i][1]);
       }
       
+      //Converting the reult into ArrayList as per requirement of submission
       List<Integer> winners = winnerSet.stream().collect(Collectors.toList());
       List<Integer> losers = loserSet.stream().collect(Collectors.toList());
       
