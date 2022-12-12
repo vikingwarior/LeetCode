a5c8238b60b307b4b4062a2de31cbcd03006db26
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-12-12 23:32:50 +0530

    Update 70.climbing-stairs.java

--- 70.climbing-stairs.java
+++ 70.climbing-stairs.java
@@ -12,7 +12,7 @@ class Solution {
       ways[0] = 1;
       ways[1] = 1;
       
-      for(int i = 2; i < n+1; i++) ways[i] = ways[i] = ways[i-1] + ways[i-2];
+      for(int i = 2; i < n+1; i++) ways[i] = ways[i-1] + ways[i-2];
       
       return ways[n];
     }
