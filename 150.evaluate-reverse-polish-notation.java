9c24fb7bcc01fda478cf7bf5fbd03d9884534819
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-12-17 23:33:25 +0530

    new file: 150.evaluate-reverse-polish-notation.java

---
+++ 150.evaluate-reverse-polish-notation.java
@@ -0,0 +1,14 @@
+class Solution {
+        public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if ("+".equals(s)) stack.push(stack.pop() + stack.pop());
+            else if ("-".equals(s)) stack.push(-stack.pop() + stack.pop());
+            else if ("/".equals(s)) stack.push((int)(1D / stack.pop() * stack.pop()));
+            else if ("*".equals(s)) stack.push(stack.pop() * stack.pop());
+            else stack.push(Integer.valueOf(s));
+        }
+
+        return stack.pop();
+    }
+}
