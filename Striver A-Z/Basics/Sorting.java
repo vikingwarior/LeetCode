3f9a2fca1ca13fa3a5c142de06357b1d6474315c
Harsh Paradkar <contact.harshparadkar@gmail.com>
2025-02-10 00:35:12 +0530

    Written code for: - Selection sort - Bubble sort - Insertion sort - Merge sort - Quick sort

---
+++ Striver A-Z/Basics/Sorting.java
@@ -0,0 +1,168 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Sorting {
+
+  /* Iterative Sorts */
+  private static void selectionSort(int[] arr) {
+    int n = arr.length;
+
+    for (int i = 0; i < n; i++) {
+      int min = i;
+
+      for (int j = i; j < n; j++) {
+        if (arr[j] < arr[min]) {
+          min = j;
+        }
+      }
+
+      swapNum(arr, i, min);
+    }
+
+  }
+
+  private static void bubbleSort(int[] arr) {
+    int n = arr.length;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          swapNum(arr, j, j + 1);
+        }
+      }
+    }
+
+  }
+
+  private static void insertionSort(int[] arr) {
+    int n = arr.length;
+    int j;
+    for (int i = 0; i < arr.length; i++) {
+      j = i;
+      while (j > 0 && arr[j - 1] > arr[j]) {
+        swapNum(arr, j, --j);
+      }
+    }
+  }
+
+  /* Recursive Sorts */
+
+  /* Merge Sort */
+  private static void mergeSort(int[] arr) { // Divide and Merge
+    divide(arr, 0, arr.length - 1);
+  }
+
+  public static void divide(int[] arr, int low, int high) {
+    if (low >= high)
+      return;
+    int mid = (low + high) / 2;
+    divide(arr, low, mid);
+    divide(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+  }
+
+  private static void merge(int[] arr, int low, int mid, int high) {
+    ArrayList<Integer> temp = new ArrayList<>();
+    int left = low;
+    int right = mid + 1;
+
+    while (left <= mid && right <= high) {
+      if (arr[left] <= arr[right]) {
+        temp.add(arr[left]);
+        left++;
+      } else {
+        temp.add(arr[right]);
+        right++;
+      }
+    }
+
+    while (left <= mid) {
+      temp.add(arr[left]);
+      left++;
+    }
+
+    while (right <= high) {
+      temp.add(arr[right]);
+      right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+      arr[i] = temp.get(i - low);
+    }
+  }
+
+  /*
+   * Quick Sort
+   * Reason to use above merge -> Auxilary array isnt required in merge.
+   */
+
+  /*
+   * Algorithm ->
+   * - Pick a pivot
+   * - Put the pivot in its correct place
+   * - correct place means all nos smaller than no should be on left and all
+   * larger nos should be on the right
+   * 
+   */
+  private static void quickSort(int[] arr) {
+    quickSort(arr, 0, arr.length - 1);
+  }
+
+  private static void quickSort(int[] arr, int low, int high) {
+    if (low < high) {
+      int partitionIndex = partition(arr, low, high);
+      quickSort(arr, low, partitionIndex - 1);
+      quickSort(arr, partitionIndex + 1, high);
+    }
+  }
+
+  private static int partition(int[] arr, int low, int high) {
+    int pivotNo = arr[low];
+    int left = low;
+    int right = high;
+
+
+    while (left < right) {
+      while (arr[left] <= pivotNo && left <= high-1) {
+        left++;
+      }
+      while (arr[right] > pivotNo && right > low+1) {
+        right--;
+      }
+
+      if (left < right) {
+        swapNum(arr, left, right);
+      }
+    }
+
+    swapNum(arr, low, right);
+    return right;
+  }
+
+  /* Main Function */
+  public static void main(String[] args) {
+    // int[] nums = { 24, 13};
+    int[] nums = { 13, 46, 24, 52, 20, 9 };
+    printArr(nums);
+    quickSort(nums);
+    printArr(nums);
+  }
+
+  /* Helper Methods */
+  private static void printArr(int[] nums) {
+    for (int i : nums) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+  }
+
+  private static void swapNum(int[] arr, int n1, int n2) {
+    arr[n1] += arr[n2];
+    arr[n2] = arr[n1] - arr[n2];
+    arr[n1] -= arr[n2];
+  }
+
+  private static int[] swapNum(int n1, int n2) {
+    return new int[] { n2, n1 };
+  }
+}
